a7811cc1df883b78a5004f09e9c2e256a08d19eb
CornerYoung <947498533@qq.com>
2020-03-18 16:26:15 +0800

    xi项目搭建

---
+++ README.md
@@ -0,0 +1,6 @@
+## 安装相关依赖
+
+mobx <br>
+mobx-react <br>
+@babel/plugin-proposal-decorators <br>
+@babel/plugin-proposal-class-properties
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      hello mobx
+    </div>
+  );
+}
+
+export default App;
