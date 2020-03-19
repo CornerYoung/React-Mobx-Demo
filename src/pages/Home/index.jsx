0af3241079ffa4151286b9c0e4c0e4e071205529
CornerYoung <947498533@qq.com>
2020-03-19 21:09:38 +0800

    操作按钮点击事件，实现页面的任务添加删除和重置

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { inject,observer} from 'mobx-react'
+import store from '../../store';
 
 @inject('store') //把store注入进来
 @observer //对注入进来的store进行观察，如果store发生变化，home组件中页相应发生变化
@@ -11,12 +12,37 @@ class Home extends Component {
         this.state = {}
     }
 
+    handleTodos = (type)=>{
+        const { store } = this.props
+        switch (type) {
+            case 'add':
+                store.addTodo('新增一条任务')
+                break
+            case 'delete':
+                store.deleteTodo()
+                break
+            case 'reset':
+                store.resetTodo()
+                break
+            default :
+                return
+        }
+    }
+
     render() {
         const {store} = this.props
         return (
             <div>
                 <h1>Mobx使用</h1>
                 <div>{store.time}</div>
+                <button onClick={() => { this.handleTodos('add')}}>添加一条任务</button>
+                <button onClick={() => { this.handleTodos('delete') }}>删除一条任务</button>
+                <button onClick={() => { this.handleTodos('reset') }}>重置任务列表</button>
+                <ul>
+                    {
+                        store.todos.map((ele, index) => <li key={ele + index}>{ele}</li>)
+                    }
+                </ul>
             </div>
         );
     }
