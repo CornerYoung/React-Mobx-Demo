2d52434282a383c4a07d80712bd45e5ed0a8a9e6
CornerYoung <947498533@qq.com>
2020-03-19 21:22:00 +0800

    computed装饰器计算任务条数并进行实时更新

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,12 @@
-import { observable, computed, action } from "mobx";
+import { observable, computed, action } from "mobx"
+import moment from 'moment'
 
 class AppStore {
-    @observable time = '2020'
+    @observable time = ''
     @observable todos = []
+    @computed get desc() {
+        return `${this.time} 还有${this.todos.length}条任务待完成`
+    }
     @action addTodo(todo){
         this.todos.push(todo)
     }
@@ -12,8 +16,15 @@ class AppStore {
     @action resetTodo() {
         this.todos = []
     }
+    @action getNowTime(){
+        this.time = moment().format('YYYY-MM-DD HH:mm:ss')
+    }
 }
 
 const store = new AppStore()
+
+setInterval(()=>{
+    store.getNowTime()
+},1000)
 
 export default store
